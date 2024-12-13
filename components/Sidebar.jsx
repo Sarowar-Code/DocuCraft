c0efc68c4cde9b82486632e0d89299850017bcdd
Sarowar-h <sarw838dev@gmail.com>
2024-12-13 10:44:24 +0600

    created nested route

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,10 +1,59 @@
+"use client";
+import {
+    getDocumentsByAuthor,
+    getDocumentsByCategory,
+    getDocumentsByTag,
+} from "@/utils/doc-util";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 const Sidebar = ({ docs }) => {
-    const roots = docs.filter((doc) => !doc.parent);
-    const nonRoots = Object.groupBy(
-        docs.filter((doc) => doc.parent),
-        ({ parent }) => parent
-    );
+    const pathName = usePathname();
+    const [rootNodes, setRootNodes] = useState([]);
+    const [nonRootNodesGrouped, setNonRootNodeGrouped] = useState({});
+
+    useEffect(() => {
+        let matchedDocs = docs;
+
+        if (pathName.includes("/tags")) {
+            const tag = pathName.split("/")[2];
+            matchedDocs = getDocumentsByTag(docs, tag);
+        } else if (pathName.includes("/author")) {
+            const author = pathName.split("/")[2];
+            matchedDocs = getDocumentsByAuthor(docs, author);
+        } else if (pathName.includes("/categories")) {
+            const category = pathName.split("/")[2];
+            matchedDocs = getDocumentsByCategory(docs, category);
+        }
+
+        const roots = matchedDocs.filter((doc) => !doc.parent);
+        const nonRoots = Object.groupBy(
+            matchedDocs.filter((doc) => doc.parent),
+            ({ parent }) => parent
+        );
+
+        const nonRootKeys = Reflect.ownKeys(nonRoots);
+        nonRootKeys.forEach((key) => {
+            const foundInRoots = roots.find((root) => root.id === key);
+            if (!foundInRoots) {
+                const foundInDocs = docs.find((doc) => doc.id === key);
+                roots.push(foundInDocs);
+            }
+        });
+
+        roots.sort((a, b) => {
+            if (a.order < b.order) {
+                return -1;
+            }
+            if (a.order > b.order) {
+                return 1;
+            }
+            return 0;
+        });
+
+        setRootNodes([...roots]);
+        setNonRootNodeGrouped({ ...nonRoots });
+    }, [pathName]);
 
     return (
         <nav className="lg:block my-10">
@@ -14,7 +63,7 @@ const Sidebar = ({ docs }) => {
                     <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
                     <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
                     <ul role="list" className="border-l border-transparent">
-                        {roots.map((rootNode) => (
+                        {rootNodes.map((rootNode) => (
                             <li key={rootNode.id} className="relative">
                                 <Link
                                     aria-current="page"
@@ -25,12 +74,12 @@ const Sidebar = ({ docs }) => {
                                         {rootNode.title}
                                     </span>
                                 </Link>
-                                {nonRoots[rootNode.id] && (
+                                {nonRootNodesGrouped[rootNode.id] && (
                                     <ul
                                         role="list"
                                         className="border-l border-transparent"
                                     >
-                                        {nonRoots[rootNode.id].map(
+                                        {nonRootNodesGrouped[rootNode.id].map(
                                             (subRoot) => (
                                                 <li key={subRoot.id}>
                                                     <Link
